046b9891ef11b3e80e8541a596d4f1c9d618f5a4
Hubert Podlewski <h.podlewski@stud.elka.pw.edu.pl>
2020-12-02 21:26:31 +0100

    Added right side panel

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 import java.util.Map;
+import javafx.scene.control.Button; 
 import java.util.HashMap;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
@@ -9,23 +9,24 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
-
 public class App extends Application{
-    static final int WIDTH = 4;
-    static final int HEIGHT = 4;
+    static final int WIDTH = 16;
+    static final int HEIGHT = 8;
     static final int SQUARE_SIZE = 100;
-    static final int SCOREBAR_WIDTH = 120;
+    static final int SCOREBAR_WIDTH = 150;
 
     private static Map<Integer, Color> colorDict = new HashMap<>(); // can be later used for coloring squares 
 
     public static void main(String[] args){
-        colorDict.put(0, Color.GREEN);
-        colorDict.put(1, Color.BLUE);
-        colorDict.put(2, Color.YELLOW);
-        colorDict.put(3, Color.RED);
+        colorDict.put(1, Color.GREEN);
+        colorDict.put(2, Color.BLUE);
+        colorDict.put(3, Color.YELLOW);
+        colorDict.put(4, Color.RED);
         launch(args);
     }
 
@@ -39,17 +40,31 @@ public class App extends Application{
     public void start(Stage primaryStage){
 
         primaryStage.setTitle("Title");
-        
-        Pane layout = new Pane();
-        layout.setPrefSize(WIDTH*SQUARE_SIZE, HEIGHT*SQUARE_SIZE);
-   
-        Scene scene = new Scene(layout);
+
+        Pane gameLayout = new Pane();
+        gameLayout.setPrefSize(WIDTH*SQUARE_SIZE, HEIGHT*SQUARE_SIZE);
+        HBox wholeLayout = new HBox();
+        wholeLayout.setPrefSize(WIDTH*SQUARE_SIZE + SCOREBAR_WIDTH, HEIGHT*SQUARE_SIZE);
+        VBox scorebarLayout = new VBox();
+        scorebarLayout.setPrefSize(SCOREBAR_WIDTH, HEIGHT*SQUARE_SIZE);
+
+        Text scoreLabel = new Text("Score");
+        scoreLabel.setFont(new Font("Arial", 35));
+
+        Text score = new Text("00000000");
+        score.setFont(new Font("Comic Sans", 24));
+
+        scorebarLayout.getChildren().add(scoreLabel);
+        scorebarLayout.getChildren().add(score);    
+
+        wholeLayout.getChildren().add(gameLayout);
+        wholeLayout.getChildren().add(scorebarLayout);
+        Scene scene = new Scene(wholeLayout);
         
         primaryStage.setScene(scene);
        
         for(int x = 0; x < WIDTH; x++){
             for(int y = 0; y < HEIGHT; y++){
-                //Square sqr = new Square(x,y,1);
                 Rectangle rctg = new Rectangle(SQUARE_SIZE, SQUARE_SIZE, Color.GREEN);
                 rctg.setTranslateX(SQUARE_SIZE*x);
                 rctg.setTranslateY(SQUARE_SIZE*y);
@@ -57,25 +72,22 @@ public class App extends Application{
                 rctg.setStroke(Color.BLACK);
 
                 rectangles[y*WIDTH + x] = rctg;
-                layout.getChildren().add(rctg);
+                gameLayout.getChildren().add(rctg);
 
                 rctg.setOnMouseClicked(new EventHandler<MouseEvent>(){
                     @Override
                     public void handle(MouseEvent event){
                         int xOfRctg = (int)(event.getSceneX())/SQUARE_SIZE; //x coordinate of the rectangle 
                         int yOfRctg = (int)(event.getSceneY())/SQUARE_SIZE; // y -|| -
-                        // ^those tell you which rectangle was pressed, useful for implementing game mechanics 
-                        System.out.println(getRectangleAt(xOfRctg, yOfRctg).getFill());
+                        // ^those tell you which rectangle was pressed, useful for implementing game mechanics
                         rctg.setFill(Color.RED);
                     }
                 });
             }
         }
 
-        getRectangleAt(1, 2).setFill(Color.BEIGE);
+        getRectangleAt(1, 2).setFill(Color.BEIGE); //example use of getRectangleAt()
         primaryStage.show();
     }
-
-
 
 }
