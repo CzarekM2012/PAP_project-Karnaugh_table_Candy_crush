2f1032eff2796dfc4f28ec8f6a1cb38afbbfd005
Alek148 <38920346+Alek148@users.noreply.github.com>
2020-12-03 22:19:59 +0100

    Bound GUI to KarnaughTable, added tile highlight

--- Karnaugh/src/App.java
+++ Karnaugh/src/App.java
@@ -1,53 +1,215 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.BoxLayout;
-import javax.swing.SwingUtilities;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Container;
-import java.awt.Component;
-import java.awt.Font;
-import java.awt.Dimension;
+package Karnaugh.src;
 
-class ButtonListener implements ActionListener {
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        ((JButton) event.getSource()).setText("imPressed!");
-    }
-}
+import java.util.Map;
 
-public class App {
+import javax.swing.text.Highlighter.Highlight;
+
+import javafx.scene.control.Button;
+import java.util.HashMap;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class App extends Application {
+
+    // Karnaug table setup values
+    static final int startTableSizeXBits = 3;
+    static final int startTableSizeYBits = 3;
+    static final int startTableValueCount = 3;
+
+    static int WIDTH = 1 << startTableSizeXBits; // = 2^startTableSizeXBits
+    static int HEIGHT = 1 << startTableSizeYBits;
+    static int SQUARE_SIZE = 100;
+    static int SCOREBAR_WIDTH = 150;
+
+    static Coord lastSelectedTile = null;
+
+    private static Map<Integer, Color> colorDict = new HashMap<>(); // can be later used for coloring squares
+    KarnaughTable karnaugh; // Main karnaugh table, this is the core element of the game
+    ArrayList<Coord> highlightedTiles = new ArrayList<>();
+
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("First Button");
-            Container pane = frame.getContentPane();
-            frame.setBounds(0, 0, 500, 500); // window size setting
-
-            // Finish after the window has been closed
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            BoxLayout mainLayout = new BoxLayout(pane, BoxLayout.Y_AXIS);
-            pane.setLayout(mainLayout);
-
-            // Hello text field
-            JTextField helloTextField = new JTextField("Hello World!");
-            Font font = new Font("SansSerif", Font.BOLD, 70);
-            helloTextField.setAlignmentX(Component.CENTER_ALIGNMENT);
-            helloTextField.setEditable(false);
-            helloTextField.setFont(font);
-            helloTextField.setHorizontalAlignment(JTextField.CENTER);
-            pane.add(helloTextField);
-
-            // Button
-            JButton closeButton = new JButton("Not yet pressed");
-            closeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-            closeButton.addActionListener(new ButtonListener());
-            closeButton.setPreferredSize(new Dimension(250, 100));
-            pane.add(closeButton);
-
-            // frame.pack();
-            frame.setVisible(true); // pokaż onkno
-        });
+        colorDict.put(0, Color.GREEN);
+        colorDict.put(1, Color.BLUE);
+        colorDict.put(2, Color.YELLOW);
+        colorDict.put(3, Color.RED);
+        launch(args);
     }
+
+    public Rectangle[] rectangles = new Rectangle[WIDTH * HEIGHT];
+
+    public Rectangle getRectangleAt(int xRctg, int yRctg) {
+        // returns a reference to a rectangle on the board
+        return rectangles[yRctg * WIDTH + xRctg];
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        // Preparing window
+        primaryStage.setTitle("Karnaugh");
+
+        Pane gameLayout = new Pane();
+        gameLayout.setPrefSize(WIDTH * SQUARE_SIZE, HEIGHT * SQUARE_SIZE);
+        HBox wholeLayout = new HBox();
+        wholeLayout.setPrefSize(WIDTH * SQUARE_SIZE + SCOREBAR_WIDTH, HEIGHT * SQUARE_SIZE);
+        VBox scorebarLayout = new VBox();
+        scorebarLayout.setPrefSize(SCOREBAR_WIDTH, HEIGHT * SQUARE_SIZE);
+
+        
+        // Creating layout elements
+        Button menuButton = new Button("Main menu");
+        menuButton.setFont(new Font("Arial", 15));
+        menuButton.setPrefWidth(0.8 * SCOREBAR_WIDTH);
+        menuButton.setTextFill(Color.WHITE);
+        menuButton.setCursor(Cursor.HAND);
+        // makes the button red, could probably make it easier with css
+        menuButton.setBackground(new Background(new BackgroundFill(Color.RED, new CornerRadii(0), Insets.EMPTY))); 
+
+        menuButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Main menu button clicked.");
+                // currently unused, as there's no main menu yet
+            }
+        });
+
+        Text scoreLabel = new Text("Score:");
+        scoreLabel.setFont(new Font("Arial", 35));
+
+        Text score = new Text("00000000");
+        score.setFont(new Font("Comic Sans", 24));
+
+        scorebarLayout.setAlignment(Pos.BASELINE_CENTER);
+        scorebarLayout.getChildren().add(menuButton);
+        scorebarLayout.getChildren().add(scoreLabel);
+        scorebarLayout.getChildren().add(score);
+
+        wholeLayout.getChildren().add(gameLayout);
+        wholeLayout.getChildren().add(scorebarLayout);
+        Scene scene = new Scene(wholeLayout);
+
+        primaryStage.setScene(scene);
+
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                Rectangle rctg = new Rectangle(SQUARE_SIZE, SQUARE_SIZE, Color.GREEN);
+                rctg.setTranslateX(SQUARE_SIZE * x);
+                rctg.setTranslateY(SQUARE_SIZE * y);
+                rctg.setCursor(Cursor.HAND);
+                rctg.setStroke(Color.BLACK);
+
+                rectangles[y * WIDTH + x] = rctg;
+                gameLayout.getChildren().add(rctg);
+
+                rctg.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        int xOfRctg = (int) (event.getSceneX()) / SQUARE_SIZE; // x coordinate of the rectangle
+                        int yOfRctg = (int) (event.getSceneY()) / SQUARE_SIZE; // y -|| -
+                        // ^those tell you which rectangle was pressed, useful for implementing game
+                        // mechanics
+                        System.out.println("Clicked (" + xOfRctg + ", " + yOfRctg + ")");
+                        onTileSelected(xOfRctg, yOfRctg);
+                    }
+                });
+            }
+        }
+
+        //getRectangleAt(1, 1).setFill(Color.BEIGE); // example use of getRectangleAt()
+        //getRectangleAt(WIDTH - 2, HEIGHT - 2).setFill(Color.BEIGE); // example use of getRectangleAt()
+
+        // Initialising mechanics
+        karnaugh = new KarnaughTable(startTableSizeXBits, startTableSizeYBits, startTableValueCount);
+        updateTable();
+
+        primaryStage.show();
+    }
+
+    void setTileColor(int x, int y, Color color) {
+        getRectangleAt(x, y).setFill(color);
+    }
+
+    void highlightTile(int x, int y) {
+        System.out.println("Highlighing " + x + ", " + y);
+        highlightedTiles.add(new Coord(x, y));
+
+        Rectangle tile = getRectangleAt(x, y);
+        Color color = (Color)tile.getFill();
+        color = color.desaturate();
+        color = color.desaturate();
+        color = color.desaturate();
+        tile.setFill(color);
+    }
+
+    void highlightTiles(ArrayList<Coord> coords) {
+        for(Coord tile : coords)
+            highlightTile(tile.x, tile.y);
+    }
+
+    void highlightNeighbours(int x, int y) {
+        highlightTiles(karnaugh.AdjacentFields(new Coord(x, y)));
+    }
+
+    void removeHighlights() {
+        for(Coord tile : highlightedTiles)
+            updateTile(tile); // reset color
+        highlightedTiles.clear();
+    }
+
+    void updateTile(int x, int y) {
+        Color color = colorDict.get(karnaugh.getTileValue(x, y));
+        setTileColor(x, y, color);
+    }
+
+    void updateTile(Coord coord) {
+        updateTile(coord.x, coord.y);
+    }
+
+    void updateTiles(ArrayList<Coord> coords) {
+        for(Coord coord : coords) {
+            updateTile(coord);
+        }
+    }
+
+    void updateTable() {
+        for(int x = 0; x < karnaugh.getSizeX(); ++x)
+            for(int y = 0; y < karnaugh.getBitSizeY(); ++y)
+                updateTile(x, y);
+    }
+
+    void onTileSelected(int x, int y) {
+        
+        if(lastSelectedTile == null) {
+            lastSelectedTile = new Coord(x, y);
+            //highlightTile(x, y);
+            highlightNeighbours(x, y);
+            return;
+        }
+
+        //highlightTile(x, y);
+        trySwapTiles(new Coord(lastSelectedTile), new Coord(x, y));
+        lastSelectedTile = null;
+    }
+
+    void trySwapTiles(Coord firstTile, Coord secondTile) {
+        //highlightNeighbours(firstTile.x, firstTile.y);
+
+        removeHighlights();
+    }
+
 }
---
+++ Karnaugh/src/Coord.java
@@ -0,0 +1,40 @@
+package Karnaugh.src;
+
+class Coord {
+    public int x, y;
+
+    public Coord() {
+        set(-1, -1);
+    }
+
+    public Coord(int x, int y) {
+        set(x, y);
+    }
+
+    public Coord(Coord coord) {
+        set(coord);
+    }
+
+    public void addTo(Coord addendCoord) {
+        x += addendCoord.x;
+        y += addendCoord.y;
+    }
+
+    public Coord add(Coord addendCoord) {
+        return new Coord(x + addendCoord.x, y + addendCoord.y);
+    }
+
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void set(Coord coord) {
+        this.x = coord.x;
+        this.y = coord.y;
+    }
+
+    public boolean equals(Coord coord) {
+        return (this.x == coord.x && this.y == coord.y);
+    }
+}
