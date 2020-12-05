7dedec3444484ce1d60f34eecc457379caef6250
cmoczuls <c.moczulski.elka.pw.edu.pl>
2020-12-05 17:00:09 +0100

    Dodano funkcję do poprawnej generacji początkowej planszy i zmieniono układ plików aby odpowiadał temu w branch prototype

--- Karnaugh/src/ReplacementSource.java
+++ Karnaugh/src/ReplacementSource.java
@@ -1,9 +1,11 @@
-public enum ReplacementSource
-{
+package Karnaugh.src;
+
+public enum ReplacementSource {
     Improper(-1), Top(1), Bottom(2), Left(3), Right(4);
+
     int id;
-    private ReplacementSource(int id)
-    {
+
+    private ReplacementSource(int id) {
         this.id = id;
     }
 }
